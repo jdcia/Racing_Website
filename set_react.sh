d23c07ebe63d022d20958cfac4602d133895a1bd
jacob <jacob.garcia@student.nmt.edu>
2020-07-20 20:28:05 -0600

    Got hello world working react.

---
+++ set_react.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Setting up npms react.
+
+npm install react react-dom --save #install react
+
+npm install webpack webpack-dev-server webpack-cli --save #install webpack
+
+npm install babel-core babel-preset-env babel-preset-react html-webpack-plugin --save-dev #install babble
+
+npm install babel-loader@7 #babel loader
+
+npm install babel-preset-es2015 #random library that the error told me to download.
