82172314fb852f0d3c4259c3b687e751e091d65d
jacob <jacob.garcia@student.nmt.edu>
2020-07-21 00:14:17 -0600

    Moved the nav bar still needs some css work.

--- react_website/js_files/Navbar.js
+++ react_website/js_files/Navbar.js
@@ -6,34 +6,34 @@ class Navbar extends Component{
     render(){
        return(
          <div class="nav">
-         <div class="left">
+            <div class="links">
 
-             <div class="nav_link">
-                 Home
-             </div>
+                <div class="nav_link">
+                    Home
+                </div>
 
-             <div class="nav_link">
-                 Pigeons
-             </div>
+                <div class="nav_link">
+                    Pigeons
+                </div>
 
-             <div class="nav_link">
-                 Methods
-             </div>
+                <div class="nav_link">
+                    Methods
+                </div>
 
-             <div class="nav_link">
-                 About
-             </div>
+                <div class="nav_link">
+                    About
+                </div>
 
-             <div class="nav_link">
-                 Contact Us
-             </div>
-         </div>
+                <div class="nav_link">
+                    Contact Us
+                </div>
+            </div>
 
-         <div class="right">
-             <div class="logo">
-                 <div>LOGO HERE</div>
-             </div>
-         </div>
+            <div class="right">
+                <div class="logo">
+                    <div>LOGO HERE</div>
+                </div>
+            </div>
 
      </div>
        );
