8984a0a66b587a3f6ef24def66219e79ca243fad
jacob <jacob.garcia@student.nmt.edu>
2020-08-01 23:17:45 -0600

    saving for the night.

--- react_website/js_files/pages/Pigeon.js
+++ react_website/js_files/pages/Pigeon.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
+import Pigeon_unit from './Pigeon_unit.js';
 
 import '../../css_files/pages/Pigeon.css';
 
 class Pigeon extends Component{
+
+
+    componentDidMount(){
+        
+    }
+
     render(){
        return(
         <div className="pigeon_page">
-            
+
             <div className="pigeons_container">
-                <pigeon_unit/>
 
-                <pigeon_unit/>
+                <Pigeon_unit/>
 
-                <pigeon_unit/>
+                <Pigeon_unit/>
+
+                <Pigeon_unit/>
 
             </div>
         </div>
---
+++ react_website/js_files/pages/Pigeon_unit.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import '../../css_files/pages/Pigeon.css';
+
+class Pigeon_unit extends Component{
+
+    constructor(prop){
+        super(prop);
+        //Do stuff with prop
+    }
+
+
+    render(){
+       return(
+        <div className="unit">
+            Test
+        </div>
+       );
+    }
+ }
+ export default Pigeon_unit; 
